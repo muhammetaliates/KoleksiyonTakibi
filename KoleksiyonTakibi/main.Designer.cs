a5a07b2c1faf02843fc3d03b83cf61863080b3a8
muhammetaliates <69991125+muhammetaliates@users.noreply.github.com>
2020-08-21 22:52:34 +0300

    Update main.Designer.cs

--- KoleksiyonTakibi/main.Designer.cs
+++ KoleksiyonTakibi/main.Designer.cs
@@ -1,4 +1,25 @@
-﻿namespace KoleksiyonTakibi
+/**************************
+
+**	SAKARYA ÜNİVERSİTESİ
+**	BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+
+**	BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+
+**	NESNEYE DAYALI PROGRAMLAMA DERSİ
+**	2019-2020 YAZ DÖNEMİ
+
+**
+
+**	PROJE NUMARASI.........: 01
+**	ÖĞRENCİ ADI............: MUHAMMET ALİ ATEŞ		
+
+**	ÖĞRENCİ NUMARASI.......: B151200040
+
+**	DERSİN ALINDIĞI GRUP...: A
+
+**************************/
+
+namespace KoleksiyonTakibi
 {
     partial class main
     {
