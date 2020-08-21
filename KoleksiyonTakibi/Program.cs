fe5073b3a13df581977fb7786afa19effd013216
muhammetaliates <69991125+muhammetaliates@users.noreply.github.com>
2020-08-21 22:54:28 +0300

    Update Program.cs

--- KoleksiyonTakibi/Program.cs
+++ KoleksiyonTakibi/Program.cs
@@ -4,6 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**************************
+
+**	SAKARYA ÜNİVERSİTESİ
+**	BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+
+**	BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+
+**	NESNEYE DAYALI PROGRAMLAMA DERSİ
+**	2019-2020 YAZ DÖNEMİ
+
+**
+
+**	PROJE NUMARASI.........: 01
+**	ÖĞRENCİ ADI............: MUHAMMET ALİ ATEŞ		
+
+**	ÖĞRENCİ NUMARASI.......: B151200040
+
+**	DERSİN ALINDIĞI GRUP...: A
+
+**************************/
+
+
+
 namespace KoleksiyonTakibi
 {
     static class Program
