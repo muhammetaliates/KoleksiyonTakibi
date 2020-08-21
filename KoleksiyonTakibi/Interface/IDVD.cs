ee0761505d1f67152577e710fb6c0aa813961db3
muhammetaliates <69991125+muhammetaliates@users.noreply.github.com>
2020-08-21 22:49:27 +0300

    Update IDVD.cs

--- KoleksiyonTakibi/Interface/IDVD.cs
+++ KoleksiyonTakibi/Interface/IDVD.cs
@@ -2,6 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+/**************************
+
+**	SAKARYA ÜNİVERSİTESİ
+**	BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+
+**	BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
+
+**	NESNEYE DAYALI PROGRAMLAMA DERSİ
+**	2019-2020 YAZ DÖNEMİ
+
+**
+
+**	PROJE NUMARASI.........: 01
+**	ÖĞRENCİ ADI............: MUHAMMET ALİ ATEŞ		
+
+**	ÖĞRENCİ NUMARASI.......: B151200040
+
+**	DERSİN ALINDIĞI GRUP...: A
+
+**************************/
+
+
+
 namespace KoleksiyonTakibi
 {
     interface IDVD
