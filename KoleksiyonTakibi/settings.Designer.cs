0983d03d23bc7e249ece491356840de6b0da242d
muhammetaliates <69991125+muhammetaliates@users.noreply.github.com>
2020-08-21 21:05:02 +0300

    Add files via upload

---
+++ KoleksiyonTakibi/settings.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace KoleksiyonTakibi
+{
+    partial class settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(145, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Kategori Kısıtla";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(18, 63);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(145, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 19);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Tür Kısıtla";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kısıtla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(6, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 79);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Kısıtlamayı  Kaldır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 136);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kısıtla";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(288, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 136);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kategori Kısıtlama Kaldır";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Location = new System.Drawing.Point(525, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(241, 141);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tür Kısıtlama Kaldır";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Kısıtlamayı  Kaldır";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(7, 27);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(228, 79);
+            this.listBox2.TabIndex = 0;
+            // 
+            // settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 166);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "settings";
+            this.Text = "settings";
+            this.Load += new System.EventHandler(this.settings_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
---
+++ KoleksiyonTakibi/settings.cs
@@ -0,0 +1,143 @@
+﻿using KoleksiyonTakibi.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KoleksiyonTakibi
+{
+    public partial class settings : Form
+    {
+        public settings()
+        {
+            InitializeComponent();
+        }
+        DbClass dbClass = new DbClass();
+        bool catKont = false, turKont = false;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                comboBox1.Enabled = true;
+                catKont = true;
+            }
+            else
+            {
+                catKont = false;
+                comboBox1.Enabled = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked == true)
+            {
+                turKont = true;
+                checkBox1.Checked = false;
+                checkBox1.Enabled = false;
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+            }
+            else
+            {
+                turKont = false;
+                checkBox1.Checked = false;
+                checkBox1.Enabled = true;
+                comboBox2.Enabled = false;
+            }
+        }
+        List<ComboBoxItem> comboBox1Data;
+        List<ComboBoxItem> comboBox2Data;
+        
+        public void listBoxYenile()
+        {
+            DataTable dt = null;
+            listBox1.Items.Clear();
+            dt = dbClass.select("select * from tur where blok = 1");
+            foreach (DataRow row1 in dt.Rows)
+            {
+                listBox1.Items.Add(new ListBoxItem(row1[1].ToString(), row1[0].ToString()));
+            }
+            listBox1.DisplayMember = "getValue";
+            listBox1.ValueMember = "getData";
+
+            listBox2.Items.Clear();
+            dt = dbClass.select("select * from tur2 where blok = 1");
+            foreach (DataRow row1 in dt.Rows)
+            {
+                listBox2.Items.Add(new ListBoxItem(row1[1].ToString(), row1[0].ToString()));
+            }
+            listBox2.DisplayMember = "getValue";
+            listBox2.ValueMember = "getData";
+        }
+
+        private void settings_Load(object sender, EventArgs e)
+        {
+            comboBox1Data = new List<ComboBoxItem>();
+            comboBox1.Items.Clear();
+            DataTable dt = dbClass.select("select * from tur");
+            foreach (DataRow row in dt.Rows)
+            {
+                comboBox1Data.Add(new ComboBoxItem(row[1].ToString(), row[0].ToString()));
+            }
+
+            comboBox1.DisplayMember = "getValue";
+            comboBox1.ValueMember = "getData";
+            comboBox1.DataSource = comboBox1Data;
+
+
+            listBoxYenile();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2Data = new List<ComboBoxItem>();
+            comboBox2Data.Clear();
+            string id = ((ComboBoxItem)comboBox1.SelectedItem).getData;
+            string query = "select * from tur2 where tur = " + id;
+            DataTable dt = dbClass.select(query);
+            foreach (DataRow row in dt.Rows)
+            {
+                comboBox2Data.Add(new ComboBoxItem(row[1].ToString(), row[0].ToString()));
+            }
+            comboBox2.DisplayMember = "getValue";
+            comboBox2.ValueMember = "getData";
+            comboBox2.DataSource = comboBox2Data;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int getID = Convert.ToInt32(((ListBoxItem)listBox1.SelectedItem).getData);
+            dbClass.Execute("update tur set blok = '0' where id=" + getID.ToString());
+
+            listBoxYenile();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int getID = Convert.ToInt32(((ListBoxItem)listBox2.SelectedItem).getData);
+            dbClass.Execute("update tur2 set blok = '0' where id=" + getID.ToString());
+
+            listBoxYenile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(catKont == true)
+            {
+                dbClass.Execute("update tur set blok = '1' where id=" + ((ComboBoxItem)comboBox1.SelectedItem).getData);
+            }
+            else if(turKont == true)
+            {
+                dbClass.Execute("update tur2 set blok = '1' where id=" + ((ComboBoxItem)comboBox2.SelectedItem).getData);
+            }
+
+            listBoxYenile();
+
+        }
+    }
+}
